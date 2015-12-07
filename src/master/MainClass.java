7af7539585454fc30ff48ba6fc8876423aced944
black-dagon74 <nickk.2974@gmail.com>
2015-12-07 23:56:03 +0530

    Initialize Project & Define Params
    Signed-off-by: black-dagon74 <nickk.2974@gmail.com>

---
+++ src/master/MainClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package master;
+
+/**
+ *
+ * @author nick
+ */
+public class MainClass {
+    // Init Db Connection
+    public String cl = "java.sql.DriverManager";
+    public String user = "root";
+    public String pwd = "ExDb20Tra"; //Change MySQL/MariaDb Password Here
+    public String url = "jdbc:mysql://localhost:3306/libnk";
+    // End Db Connection
+}
